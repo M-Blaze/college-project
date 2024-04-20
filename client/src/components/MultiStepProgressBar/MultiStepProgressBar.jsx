04339754d4c495be2a5a1e3e74605fb20c34a4cd
Moulik rai <raimoulik@gmail.com>
2024-04-20 07:38:02 +0545

    chore: create a multi step progress bar

---
+++ client/src/components/MultiStepProgressBar/MultiStepProgressBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./MultiStepProgressBar.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+
+const QUARTER_STEP = 16.665
+const steps = new Array(4).fill(null)
+const MultiStepProgressBar = ({ activeStep, stepHandler }) => {
+  let stepPercentage = activeStep * QUARTER_STEP
+
+  return (
+    <ProgressBar percent={stepPercentage}>
+      {
+        steps.map((_, index) => {
+          return (<Step key={index}>
+            {({ accomplished }) => (
+              <div className={`indexedStep ${accomplished ? "accomplished" : null}`} onClick={() => stepHandler((2*index) + 1)}>
+                {index + 1}
+              </div>
+            )}
+          </Step>)
+        })
+      }
+    </ProgressBar>
+  );
+};
+
+export default MultiStepProgressBar;
