c8f5af886f0b64249a4613941fc9625b8edade25
Moulik rai <raimoulik@gmail.com>
2024-04-20 21:34:30 +0545

    update: Text component

--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import useDraggable from '../hooks/useDraggable'
 
 const Text = ({ content, styles }) => {
-  const [dragStartHandler, dragHandler, dragEndHandler] = useDraggable()
+  const [dragStartHandler, dragHandler] = useDraggable()
 
   return (
-    <div draggable className="text absolute w-max select-none" onDragStart={dragStartHandler} onDrag={dragHandler} onDragEnd={dragEndHandler} style={styles}>{content}</div>
+    <div draggable className="text absolute w-max select-none" onDragStart={dragStartHandler} onDrag={dragHandler} style={styles}>{content}</div>
   )
 }
 
