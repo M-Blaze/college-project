c15fe832a324914838e4717554aa1f4b01796332
Moulik rai <raimoulik@gmail.com>
2024-04-22 09:21:52 +0545

    update: useMouseMoveCalclulator

--- client/src/hooks/useMouseMoveCalculator.js
+++ client/src/hooks/useMouseMoveCalculator.js
@@ -1,9 +1,11 @@
 import { useRef } from 'react'
 
 const useMouseMoveCalculator = () => {
-  const mouseCoordinateRef = useRef(null)
+  const mouseCoordinateRef = useRef({})
 
   const dragStartHandler = (e) => {
+    if (!e.clientX) return
+
     mouseCoordinateRef.current = {
       x: e.clientX,
       y: e.clientY
@@ -13,7 +15,7 @@ const useMouseMoveCalculator = () => {
   const dragHandler = (e) => {
     const { x, y } = mouseCoordinateRef.current
     const mouseMovedX = (x - e.clientX) 
-    const mouseMovedY = (y - e.clientY) 
+    const mouseMovedY = (y - e.clientY)
     
     return [mouseMovedX, mouseMovedY]
   }
