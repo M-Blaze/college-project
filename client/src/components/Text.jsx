5fc0fefb7333b81af01f7678205208535fee04f1
Moulik rai <raimoulik@gmail.com>
2024-04-23 21:35:06 +0545

    chore: add resizer on text component

--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+
 import useDraggable from '../hooks/useDraggable'
+import Resizer from './Resizer'
 
 const Text = ({ content, styles }) => {
   const [dragStartHandler, dragHandler] = useDraggable()
 
   return (
-    <div draggable className="text absolute w-max select-none" onDragStart={dragStartHandler} onDrag={dragHandler} style={styles}>{content}</div>
+    <div draggable className="text absolute w-max" onDragStart={dragStartHandler} onDrag={dragHandler} style={styles}>
+      <Resizer>
+        <div className="text-content w-full h-full outline-none">{content}</div>
+      </Resizer>
+    </div>
   )
 }
 
