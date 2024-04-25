8988db6179b85aa0ca0dbca1502cd050f39ac218
Moulik rai <raimoulik@gmail.com>
2024-04-25 09:40:47 +0545

    chore: add ref in text component

--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 import useDraggable from '../hooks/useDraggable'
 import Resizer from './Resizer'
 
-const Text = ({ content, styles }) => {
+const Text = React.forwardRef(({ content, styles }, ref) => {
   const [dragStartHandler, dragHandler] = useDraggable()
 
   return (
-    <div draggable className="text absolute w-max" onDragStart={dragStartHandler} onDrag={dragHandler} style={styles}>
+    <div ref={ref} draggable className="text absolute w-max" onDragStart={dragStartHandler} onDrag={dragHandler} style={styles}>
       <Resizer>
         <div className="text-content w-full h-full outline-none">{content}</div>
       </Resizer>
     </div>
   )
-}
+})
 
 export default Text
