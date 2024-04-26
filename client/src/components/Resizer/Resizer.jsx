dc15ad9ee451adf5cf823147dcb6ed9743c7bb7b
Moulik rai <raimoulik@gmail.com>
2024-04-26 22:24:10 +0545

    update: add auto height on Resizer

--- client/src/components/Resizer/Resizer.jsx
+++ client/src/components/Resizer/Resizer.jsx
@@ -3,7 +3,7 @@ import React, { useState, useRef } from 'react'
 import ResizerControl from './components/ResizerControl'
 import { useMemo } from 'react'
 
-const Resizer = ({ children }) => {
+const Resizer = ({ children, autoHeight }) => {
   const [isResizable, setIsResizable] = useState(false)
   const wrapperRef = useRef()
 
@@ -11,7 +11,7 @@ const Resizer = ({ children }) => {
     if (isResizable) {
       return (
         <>
-          <div className="border-box border-2 border-purple-500 absolute top-0 left-0 right-0 bottom-0" />
+          <div className="border-box border-2 border-purple-700 absolute top-0 left-0 right-0 bottom-0" />
           <ResizerControl type="top-left" parentRef={wrapperRef} />
           <ResizerControl type="top-right" parentRef={wrapperRef} />
           <ResizerControl type="bottom-left" parentRef={wrapperRef} />
@@ -23,7 +23,7 @@ const Resizer = ({ children }) => {
   }, [isResizable])
 
   return (
-    <div tabIndex={1} onFocus={() => setIsResizable(true)} onBlur={() => setIsResizable(false)} ref={wrapperRef} className="resize-container relative">
+    <div tabIndex={1} onFocus={() => setIsResizable(true)} onBlur={() => setIsResizable(false)} ref={wrapperRef} className={`resize-container relative ${autoHeight && '!h-auto'}`}>
       {resizeControls}
       {children}
   </div>
