9187d37cea261d864b6db52d785f33365045ad4a
Moulik rai <raimoulik@gmail.com>
2024-04-26 22:24:53 +0545

    update: add autoHeight prop on Resizer in EditableText

--- client/src/components/EditableText.jsx
+++ client/src/components/EditableText.jsx
@@ -7,13 +7,19 @@ const EditableText = ({ textData, setActive }) => {
   const [dragStartHandler, dragHandler] = useDraggable()
 
   const onDragStart = (e) => {
+    e.stopPropagation()
     setActive()
     dragStartHandler(e)
   }
 
+  const onClick = (e) => {
+    e.stopPropagation()
+    setActive()
+  }
+
   return (
-    <div draggable className="text absolute w-max" onClick={setActive} onDragStart={onDragStart} onDrag={dragHandler} style={textData.styles}>
-      <Resizer>
+    <div draggable className="text absolute w-max" onClick={onClick} onDragStart={onDragStart} onDrag={dragHandler} style={textData.styles}>
+      <Resizer autoHeight>
         <div className="text-content w-full h-full outline-none">{textData.content}</div>
       </Resizer>
     </div>
