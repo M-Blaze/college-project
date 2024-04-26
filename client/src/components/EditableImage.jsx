70541ea647324613eacaee57e98ccd438cddc9b5
Moulik rai <raimoulik@gmail.com>
2024-04-26 08:27:38 +0545

    refactor: abstract components

---
+++ client/src/components/EditableImage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import useDraggable from '../hooks/useDraggable'
+import Resizer from './Resizer'
+
+const EditableImage = ({ src, styles, setActive }) => {
+  const [dragStartHandler, dragHandler] = useDraggable()
+  
+  const onDragStart = (e) => {
+    setActive()
+    dragStartHandler(e)
+  }
+  
+  return (
+    <div draggable className="img-holder absolute w-max" onClick={setActive} onDragStart={onDragStart} onDrag={dragHandler} style={styles} >
+      <Resizer>
+        <img draggable={false} src={src} alt={src} /> 
+      </Resizer>
+    </div>
+  )
+}
+
+export default EditableImage
--- client/src/components/EditableText.jsx
+++ client/src/components/EditableText.jsx
@@ -1,68 +1,22 @@
-import React, { useState } from 'react'
-import { Baseline, Bold, Pencil, Trash2 } from 'lucide-react';
-import { usePopperTooltip } from 'react-popper-tooltip';
-import 'react-popper-tooltip/dist/styles.css';
+import React from 'react'
 
-import Text from './Text';
+import useDraggable from '../hooks/useDraggable'
+import Resizer from './Resizer'
 
-const IconWrap = ({ children }) => {
+const EditableText = ({ textData, setActive }) => {
+  const [dragStartHandler, dragHandler] = useDraggable()
+
+  const onDragStart = (e) => {
+    setActive()
+    dragStartHandler(e)
+  }
+
   return (
-    <div className="icon-holder hover:bg-gray-200 cursor-pointer p-1 rounded-sm">
-      {children}
+    <div draggable className="text absolute w-max" onClick={setActive} onDragStart={onDragStart} onDrag={dragHandler} style={textData.styles}>
+      <Resizer>
+        <div className="text-content w-full h-full outline-none">{textData.content}</div>
+      </Resizer>
     </div>
-  )
-}
-
-const EditableText = ({ textData }) => {
-  const {
-    getArrowProps,
-    getTooltipProps,
-    setTooltipRef,
-    setTriggerRef,
-    visible
-  } = usePopperTooltip({
-    placement: "top",
-    trigger: "click",
-    interactive: true,
-    offset: [10, 20]
-  });
-
-  return (
-    <>
-      <Text ref={setTriggerRef} styles={textData.styles} content={textData.content} />
-      {visible && (
-        <div
-          ref={setTooltipRef}
-          {...getTooltipProps({ className: 'tooltip-container' })}
-        >
-          <div {...getArrowProps({ className: 'tooltip-arrow' })} />
-          <div className="edit-options bg-white shadow-lg rounded-md p-2">
-            <ul className="options-list flex">
-              <li>
-                <IconWrap>
-                  <Baseline className='h-4 w-4'/>
-                </IconWrap>
-              </li>
-              <li>
-                <IconWrap>
-                  <Bold className='h-4 w-4' />
-                </IconWrap>
-              </li>
-              <li>
-                <IconWrap>
-                  <Pencil className='h-4 w-4' />
-                </IconWrap>
-              </li>
-              <li>
-                <IconWrap>
-                  <Trash2 className='h-4 w-4' />
-                </IconWrap>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
-    </>
   )
 }
 
