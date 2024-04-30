7c9637d6647d9c091a746696cc8f69eaf749b73e
Moulik rai <raimoulik@gmail.com>
2024-04-30 08:58:44 +0545

    chore: return list items only

--- client/src/components/TemplateEditor/components/EditorOptions.jsx
+++ client/src/components/TemplateEditor/components/EditorOptions.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import { SketchPicker } from 'react-color'
-import { Baseline, Bold, Underline, Italic, Trash2 } from 'lucide-react';
 import { usePopperTooltip } from 'react-popper-tooltip';
+import { Baseline, Bold, Underline, Italic, Trash2 } from 'lucide-react';
 import 'react-popper-tooltip/dist/styles.css';
+
 
 const IconWrap = ({ children, clickHandler }) => {
   return (
@@ -13,7 +14,7 @@ const IconWrap = ({ children, clickHandler }) => {
   )
 }
 
-const TextEditorOptions = ({ textData, updateElementStyle }) => {
+const EditorOptions = ({ addElement, editorType, updateCardStyle, backgroundColor, textData, updateElementStyle, deleteElement }) => {
   const {
     getArrowProps,
     getTooltipProps,
@@ -27,7 +28,12 @@ const TextEditorOptions = ({ textData, updateElementStyle }) => {
     offset: [10, 20]
   })
 
-  const fontSize = Number(textData.styles.fontSize.split('px')[0])
+
+  const updateBackgroundColor = (color) => {
+    updateCardStyle({ backgroundColor: color.hex })
+  }
+
+  const fontSize = Number(textData?.styles?.fontSize?.split('px')[0])
 
   const inputChangeHandler = e => {
     updateElementStyle("content", e.target.value)
@@ -46,7 +52,7 @@ const TextEditorOptions = ({ textData, updateElementStyle }) => {
     updateElementStyle("color", color.hex)
   }
 
-  return (
+  const TextEditorOptions = (
     <>
       <li className='px-1'>
         <div className="font-select">
@@ -100,15 +106,48 @@ const TextEditorOptions = ({ textData, updateElementStyle }) => {
         </IconWrap>
       </li>
       <li className="px-1 w-64">
-        <input className='w-full outline-none border-2 p-2 rounded-md' type="text" placeholder='Your text here...' value={textData.content} onChange={inputChangeHandler} />
-      </li>
-      <li className='px-1'>
-        <IconWrap>
-          <Trash2 className='h-4 w-4' />
-        </IconWrap>
+        <input className='w-full outline-none border-2 p-2 rounded-md' type="text" placeholder='Your text here...' value={textData?.content} onChange={inputChangeHandler} />
       </li>
     </>
   )
+
+  return (
+    <div className='editor-bar w-full mb-10'>
+      <ul className="editor-options-list flex items-center border-b-2 -mx-1 py-2">
+        <li className='flex items-center px-1'>
+          <div className="color-picker-trigger w-11 h-11 rounded-md cursor-pointer border-2 shadow-md" style={{ backgroundColor }} ref={setTriggerRef}></div>
+          { 
+            visible && (
+            <div ref={setTooltipRef} {...getTooltipProps({ className: 'tooltip-container' })}>
+              <div {...getArrowProps({ className: 'tooltip-arrow' })} />
+              <SketchPicker color={backgroundColor} onChangeComplete={ updateBackgroundColor } />
+            </div>)
+          }
+        </li>
+        <li className='px-1'>
+          <button className='border-2 p-2 rounded-md hover:bg-gray-200' onClick={() => addElement('text')}>Add Text</button>
+        </li>
+        <li className='px-1'>
+          <button className='border-2 p-2 rounded-md hover:bg-gray-200' onClick={() => addElement('image')}>Add Image</button>
+        </li>
+        <li className='px-1'>
+          <button className='border-2 p-2 rounded-md hover:bg-gray-200' onClick={() => addElement('image')}>Show QR</button>
+        </li>
+        {
+          editorType === 'text' && <TextEditorOptions />
+        }
+        {
+          editorType && (
+            <li className='px-1'>
+              <IconWrap clickHandler={deleteElement}>
+                <Trash2 className='h-4 w-4' />
+              </IconWrap>
+            </li>
+          )
+        }
+      </ul>
+    </div>
+  )
 }
 
-export default TextEditorOptions
+export default EditorOptions
