7ce65cb547a958dc54fcf2e013cd7b3420b06747
Moulik rai <raimoulik@gmail.com>
2024-04-30 08:58:05 +0545

    chore: add key handler in editable text

--- client/src/components/EditableText.jsx
+++ client/src/components/EditableText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import useDraggable from '../hooks/useDraggable'
 import Resizer from './Resizer'
 
-const EditableText = ({ textData, setActive }) => {
+const EditableText = ({ textData, setActive, deleteElement }) => {
   const [dragStartHandler, dragHandler] = useDraggable()
 
   const onDragStart = (e) => {
@@ -17,8 +17,41 @@ const EditableText = ({ textData, setActive }) => {
     setActive()
   }
 
+  const handleKeyDown = (e) => {
+    const KEYS = ["ArrowUp", "ArrowLeft", "ArrowDown", "ArrowRight", "Delete"]
+
+    if (!KEYS.includes(e.key)) return
+
+    if (e.key === "Delete") {
+      return deleteElement()
+    }
+    
+    let newCoordinateX, newCoordinateY
+    const x = e.target.style.left || '0px'
+    const y = e.target.style.top || '0px'
+
+    if (e.key === "ArrowUp") {
+      newCoordinateY = `calc(${y} - 1px)`
+    }
+    
+    if (e.key === "ArrowDown") {
+      newCoordinateY = `calc(${y} + 1px)`
+    }
+
+    if (e.key === "ArrowLeft") {
+      newCoordinateX = `calc(${x} - 1px)`
+    }
+
+    if (e.key === "ArrowRight") {
+      newCoordinateX = `calc(${x} + 1px)`
+    }
+
+    e.target.style.left = newCoordinateX
+    e.target.style.top = newCoordinateY
+  }
+  
   return (
-    <div draggable className="text absolute w-max" onClick={onClick} onDragStart={onDragStart} onDrag={dragHandler} style={textData.styles}>
+    <div draggable onKeyDown={handleKeyDown} className="text absolute w-max" onClick={onClick} onDragStart={onDragStart} onDrag={dragHandler} style={textData.styles}>
       <Resizer autoHeight>
         <div className="text-content w-full h-full outline-none">{textData.content}</div>
       </Resizer>
