5829bc8bdf99e7b7128b3de28665cea630f67c93
Moulik rai <raimoulik@gmail.com>
2024-04-30 20:37:01 +0545

    chore: save template method

--- client/src/components/TemplateEditor/TemplateEditor.jsx
+++ client/src/components/TemplateEditor/TemplateEditor.jsx
@@ -6,6 +6,7 @@ import EditableText from '../EditableText'
 import EditableImage from '../EditableImage'
 import TextEditorOptions from './components/TextEditorOptions'
 import CardEditorOptions from './components/CardEditorOptions'
+import { Link } from 'lucide-react';
 
 const DEFAULT_TEXT_DATA = { 
   type: "text",
@@ -33,7 +34,39 @@ const DEFAULT_IMAGE_DATA = {
   }
 }
 
-const TemplateEditor = ({ cardData }) => {
+
+const Linker = ({ link, updateLink }) => {
+  const [isLinkInputVisible, setIsLinkInputVisible] = useState(false)
+  const [href, setHref] = useState(link || '')
+
+  const saveLink = () => {
+    updateLink(href)
+    setIsLinkInputVisible(false)
+  }
+
+  if (isLinkInputVisible) {
+    return (
+      <li className='px-1 flex items-center'>
+        <div className="input-group w-64 mr-2">
+          <input className='w-full outline-none border-2 p-2 rounded-md' type="text" placeholder='Your link here...' value={href} onChange={e => setHref(e.target.value)} />
+        </div>
+        <div className="icon-wrap flex items-center p-2 border-2 rounded-md cursor-pointer hover:bg-gray-200" onClick={saveLink}>
+          <Link /><span className='ml-1'>Save</span>
+        </div>
+      </li>
+    )
+  }
+
+  return (
+    <li className='px-1 flex items-center'>
+      <div className="icon-wrap flex items-center p-2 border-2 rounded-md cursor-pointer hover:bg-gray-200" onClick={() => setIsLinkInputVisible(true)}>
+        <Link /> <span className='ml-1'>Set Link</span>
+      </div>
+    </li>
+  )
+}
+
+const TemplateEditor = ({ cardData, goToTemplateSelector, stepHandler}) => {
   const imageInputRef = useRef()
   const [cardStyles, setCardStyles] = useState(cardData.cardStyles)
   const [cardElements, setCardElements] = useState(cardData.cardElements)
@@ -159,7 +192,21 @@ const TemplateEditor = ({ cardData }) => {
 
     elements[targetIndex] = elementToUpdate
     setCardElements(elements)
-  }  
+  }
+
+  const updateMultipleStylesOfElement = (styles) => {
+    const elementToUpdate = structuredClone(activeCardElement)
+    const elements = structuredClone(cardElements)
+    const targetIndex = elements.findIndex(element => element.id === elementToUpdate.id)
+
+    elementToUpdate.styles = {
+      ...elementToUpdate.styles,
+      ...styles
+    }
+
+    elements[targetIndex] = elementToUpdate
+    setCardElements(elements)
+  }
 
   const setActiveElementHandler = (elementId, type) => {
     if (activeElement === elementId) return
@@ -167,6 +214,21 @@ const TemplateEditor = ({ cardData }) => {
     setVisibleEditor(type)
     setActiveElement(elementId)
   }
+
+  const updateLink = (link) => {
+    const elementToUpdate = structuredClone(activeCardElement)
+    const elements = structuredClone(cardElements)
+    const targetIndex = elements.findIndex(element => element.id === elementToUpdate.id)
+
+    elementToUpdate.link = link
+    elements[targetIndex] = elementToUpdate
+    setCardElements(elements)
+  }
+
+  const saveTemplate = () => {
+    console.log(cardElements)
+  }
+
 
   return (
     <div className='container'>
@@ -176,17 +238,20 @@ const TemplateEditor = ({ cardData }) => {
       <div className="content-wrap flex flex-wrap justify-center relative">
         <div className='editor-bar w-full mb-10'>
           <ul className="editor-options-list flex items-center border-b-2 -mx-1 py-2">
-            <CardEditorOptions addElement={addElement} backgroundColor={cardStyles.backgroundColor}  updateCardStyle={updateCardStyle} />
+            <CardEditorOptions addElement={addElement} backgroundColor={cardStyles.backgroundColor} updateCardStyle={updateCardStyle} />
             {
               visibleEditor === 'text' && (<TextEditorOptions updateElementStyle={updateElementStyle} textData={activeCardElement} />)
             }
+            {
+              visibleEditor && <Linker key={activeCardElement.id} link={activeCardElement.link} updateLink={updateLink} />
+            }
           </ul>
         </div>
-        <VisitingCard classes="bg-red-500" style={cardData.cardStyles} clickHandler={() => setActiveElementHandler("", "")}>
+        <VisitingCard classes="bg-red-500" style={cardStyles} clickHandler={() => setActiveElementHandler("", "")}>
           {
             cardElements.map((element, index) => {
               if (element.type === "text") {
-                return <EditableText key={element.id} textData={element} removeElement={() => removeElement(index)} setActive={() => setActiveElementHandler(element.id, "text")} />
+                return <EditableText key={element.id} textData={element} removeElement={() => removeElement(index)} setActive={() => setActiveElementHandler(element.id, "text")} updateElement={updateMultipleStylesOfElement} />
               }
 
               return <EditableImage key={element.id} removeElement={() => removeElement(index)} styles={element.styles} src={element.src} setActive={() => setActiveElementHandler(element.id)} />
@@ -194,6 +259,10 @@ const TemplateEditor = ({ cardData }) => {
           }
         </VisitingCard>
         <input ref={imageInputRef} onChange={addImageElement} type="file" className="hidden" accept="image/*" />
+      </div>
+      <div className="actions-group text-center p-10">
+        <button className='mx-2 px-5 py-3 bg-purple-600 text-white rounded-md' onClick={() => stepHandler(1)}>Go back</button>
+        <button className='mx-2 px-5 py-3 bg-red-400 text-white rounded-md' onClick={saveTemplate}>Save template</button>
       </div>
     </div>
   )
