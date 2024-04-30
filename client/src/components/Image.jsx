6eb22cafc59594115844a50e0da798e0b9f7e5e2
Moulik rai <raimoulik@gmail.com>
2024-04-30 20:32:10 +0545

    chore: open link on elements with link

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Image = ({ imageData }) => {
+  let classes = "img-holder absolute"
+  const openLink = () => {
+    if (!imageData.url) return 
+
+    window.open(imageData.url, '_blank');
+  }
+
+  if (imageData.url) {
+    classes = `${classes} cursor-pointer`
+  }
+
   return (
-    <div className="img-holder absolute" style={imageData.styles} >
+    <div className={classes} style={imageData.styles} onClick={openLink}>
       <img className='w-full h-full' src={imageData.src} alt={imageData.src} /> 
     </div>
   )
--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Text = ({ textData }) => {
+  let classes = "img-holder absolute"
+  const openLink = () => {
+    if (!textData.url) return 
+
+    window.open(textData.url, '_blank');
+  }
+
+  if (textData.url) {
+    classes = `${classes} cursor-pointer`
+  }
+  
   return (
-    <div className="text absolute w-max" style={textData.styles}>
+    <div className={classes} style={textData.styles} onClick={openLink}>
       <div className="text-content w-full h-full outline-none">{textData.content}</div>
     </div>
   )
