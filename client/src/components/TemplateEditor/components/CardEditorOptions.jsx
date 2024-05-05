ed25060debfb72ecdf90e20747ff0e79b8fe9682
Moulik rai <raimoulik@gmail.com>
2024-05-05 09:01:04 +0545

    update: remove show qr button

--- client/src/components/TemplateEditor/components/CardEditorOptions.jsx
+++ client/src/components/TemplateEditor/components/CardEditorOptions.jsx
@@ -41,9 +41,6 @@ const CardEditorOptions = ({ addElement, updateCardStyle, backgroundColor }) => 
       <li className='px-1'>
         <button className='border-2 p-2 rounded-md hover:bg-gray-200' onClick={() => addElement('image')}>Add Image</button>
       </li>
-      <li className='px-1'>
-        <button className='border-2 p-2 rounded-md hover:bg-gray-200' onClick={() => addElement('image')}>Show QR</button>
-      </li>
     </>
   )
 }
